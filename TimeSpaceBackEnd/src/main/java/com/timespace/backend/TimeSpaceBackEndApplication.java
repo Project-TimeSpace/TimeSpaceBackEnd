60f9fed221fef5ece9257fdef2e34e8d68dc4904
홍성문 <2020041412@hanyang.ac.kr>
2025-04-16 12:56:35 +0900

    init

---
+++ TimeSpaceBackEnd/src/main/java/com/timespace/backend/TimeSpaceBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.timespace.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeSpaceBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeSpaceBackEndApplication.class, args);
+	}
+
+}
---
+++ TimeSpaceBackEnd/src/test/java/com/timespace/backend/TimeSpaceBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timespace.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeSpaceBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
